634e25d117f256a5eeec257f283486f6d849abac
nicholaicascio <lordcohliani@gmail.com>
2019-04-14 15:17:54 -0600

    added menu storyboard
    someone please help i'm trapped here they're forcing me to code

---
+++ final/RazeAd/ui/MenuViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MenuViewController.swift
+//  RazeAd
+//
+//  Created by Nicholai Cascio on 4/14/19.
+//  Copyright © 2019 Razeware LLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MenuViewController: UICollectionViewController{
+
+    
+}
